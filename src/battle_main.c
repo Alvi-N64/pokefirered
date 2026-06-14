058d1ccf2def1fcde39c19e5b99a1902624a3464
Alvi N <alpi2games@gmail.com>
2026-06-14 14:13:10 +0800

    Update battle_main.c

--- src/battle_main.c
+++ src/battle_main.c
@@ -3930,8 +3930,8 @@ static void ReturnFromBattleToOverworld(void)
 #if defined(BUGFIX) || REVISION >= 0xA
             if ((gBattleOutcome == B_OUTCOME_WON) || gBattleOutcome == B_OUTCOME_CAUGHT || gBattleOutcome == B_OUTCOME_DREW)
 #else
-            if ((gBattleOutcome & B_OUTCOME_WON) || gBattleOutcome == B_OUTCOME_CAUGHT) // Bug: When Roar is used by roamer, gBattleOutcome is B_OUTCOME_PLAYER_TELEPORTED (5).
-#endif                                                                                  // & with B_OUTCOME_WON (1) will return TRUE and deactivates the roamer.
+            if ((gBattleOutcome == B_OUTCOME_WON) || gBattleOutcome == B_OUTCOME_CAUGHT || gBattleOutcome == B_OUTCOME_DREW) // Perbaikan Bug "Roar Glitch" dari versi Switch (Rev A)
+#endif                                                                                  
                 SetRoamerInactive();
         }
         m4aSongNumStop(SE_LOW_HEALTH);
